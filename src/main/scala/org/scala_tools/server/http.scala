a705483d09c13cb44754c7855c07cba70ac88d70
David Bernard <david.bernard.31@gmail.com>
2009-08-12 00:10:07 +0200

    first commit

---
+++ src/main/scala/org/scala_tools/server/http.scala
@@ -0,0 +1,260 @@
+package org.scala_tools.server
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+
+/**
+ * call eg : curl --url http://localhost:27616/add --data-binary @sample2.yaml;type=text/x-yaml
+ *
+ * @author The Netty Project (netty-dev@lists.jboss.org)
+ * @author Trustin Lee (tlee@redhat.com)
+ * @author david.bernard
+ * @based on code from org.jboss.netty.example.http.file
+ */
+object HttpServer {
+    def main(args : Array[String]) {
+      // Configure the server.
+      val bootstrap = new ServerBootstrap(
+        new NioServerSocketChannelFactory(
+          Executors.newCachedThreadPool(),
+          Executors.newCachedThreadPool()
+        )
+      )
+
+      // Set up the event pipeline factory.
+      bootstrap.setPipelineFactory(new HttpStaticFileServerPipelineFactory())
+
+      // Bind and start to accept incoming connections.
+      println("start waiting request at 27616") //TODO log
+      bootstrap.bind(new InetSocketAddress(27616))
+    }
+}
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.ChannelFutureListener;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelPipelineCoverage;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
+import org.jboss.netty.handler.codec.frame.TooLongFrameException;
+import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
+import org.jboss.netty.handler.codec.http.HttpHeaders;
+import org.jboss.netty.handler.codec.http.HttpMethod;
+import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpResponse;
+import org.jboss.netty.handler.codec.http.HttpResponseStatus;
+import org.jboss.netty.handler.codec.http.HttpVersion;
+import org.jboss.netty.handler.stream.ChunkedFile;
+
+/**
+ * @author The Netty Project (netty-dev@lists.jboss.org)
+ * @author Trustin Lee (tlee@redhat.com)
+ * @author david.bernard
+ * @based on code from org.jboss.netty.example.http.file
+ */
+@ChannelPipelineCoverage("all")
+class HttpStaticFileServerHandler(_compilerSrv : CompilerService4Group) extends SimpleChannelUpstreamHandler {
+  import java.net.URI
+
+  val sampleConfig = new SingleConfig(
+    "sample",
+    List(new File("/home/dwayne/work/oss/scala-tools/scala-tools-server/src/main/scala")),
+    List(RegExpUtil.globToRegexPattern("*.scala")),
+    Nil,
+    new File("/home/dwayne/work/oss/scala-tools/scala-tools-server/target/classes"),
+    List(
+      new File("/home/dwayne/.m2/repository/org/scala-lang/scala-library/2.7.5/scala-library-2.7.5.jar"),
+      new File("/home/dwayne/.m2/repository/org/scala-lang/scala-compiler/2.7.5/scala-compiler-2.7.5.jar"),
+      new File("/home/dwayne/.m2/repository/org/jboss/netty/netty/3.1.0.GA/netty-3.1.0.GA.jar")
+    ),
+    List(
+      "-deprecation"
+    )
+  )
+
+  override
+  def messageReceived(ctx : ChannelHandlerContext, e : MessageEvent) {
+
+    val request = e.getMessage().asInstanceOf[HttpRequest]
+    if ((request.getMethod() != HttpMethod.GET) && (request.getMethod() != HttpMethod.POST)) {
+      sendError(ctx, HttpResponseStatus.METHOD_NOT_ALLOWED, request.getMethod.toString())
+      return
+    }
+    if (request.isChunked()) {
+      sendError(ctx, HttpResponseStatus.BAD_REQUEST, "chunck not supported")
+      return
+    }
+    action(request) match {
+      case Right(txt) => sendText(ctx, txt)
+      case Left(status) => sendError(ctx, status, "")
+    }
+  }
+
+  override
+  def exceptionCaught(ctx : ChannelHandlerContext, e : ExceptionEvent) {
+    val ch = e.getChannel()
+    val cause = e.getCause()
+    if (cause .isInstanceOf[TooLongFrameException]) {
+      sendError(ctx, HttpResponseStatus.BAD_REQUEST, cause);
+      return;
+    }
+
+    cause.printStackTrace() //TODO log
+    if (ch.isConnected()) {
+      sendError(ctx, HttpResponseStatus.INTERNAL_SERVER_ERROR, cause);
+    }
+  }
+
+
+  def action(request : HttpRequest) : Either[HttpResponseStatus, CharSequence] = {
+    import org.jboss.netty.handler.codec.http.QueryStringDecoder
+    import org.yaml.snakeyaml.{Yaml, Loader}
+    import org.yaml.snakeyaml.constructor.Constructor
+
+    val decoded = new QueryStringDecoder(request.getUri)
+    val contentType = request.getHeader(HttpHeaders.Names.CONTENT_TYPE)
+
+    decoded.getPath match {
+      case "/mem" => {
+        val runtime = Runtime.getRuntime()
+        val txt = ("total memory = "+ runtime.totalMemory
+          + ", max memory = " + runtime.maxMemory
+          + ", free memory = " + runtime.freeMemory
+        )
+        Right(txt)
+      }
+      case "/add" /*if "text/x-yaml" == contentType*/ => {
+        import scala.collection.jcl.Conversions._
+        // mime-type should be text/x-yaml (see http://stackoverflow.com/questions/332129/yaml-mime-type)
+        var txt = ""
+        val yaml = new Yaml()
+        var counter = 0
+        val cfgAsYamlStr = request.getContent.toString("UTF-8")
+        println(cfgAsYamlStr)
+        val it = yaml.loadAll(cfgAsYamlStr).iterator
+        while (it.hasNext) {
+          val data = it.next().asInstanceOf[java.util.Map[String,_]]
+          def toStr(k : String, orElse : String) : String = {
+            data.get(k) match {
+              case null => orElse
+              case v => v.toString
+            }
+          }
+          def toStrList(k : String) : List[String] = {
+            data.get(k) match {
+              case null => Nil
+              case v => v.asInstanceOf[java.util.List[String]].toList
+            }
+          }
+          val cfg = new SingleConfig(
+            toStr("name", "xxx"),
+            toStrList("sourceDirs").map(new File(_)),
+            toStrList("includes").map(RegExpUtil.globToRegexPattern(_)),
+            toStrList("excludes").map(RegExpUtil.globToRegexPattern(_)),
+            new File(toStr("targetDir", "/tmp/target")),
+            toStrList("classpath").map(new File(_)),
+            toStrList("args")
+          )
+          _compilerSrv.add(new CompilerService4Single(cfg))
+          txt += "added " + data + " :: " + cfg + "\n"
+          counter += 1
+        }
+        txt += "nb compiler added/total : " + counter+ "/" + _compilerSrv.size + " ... " + contentType + "\n"
+        Right(txt)
+      }
+      // case "/add" => {
+      //   Right("only compiler definition in 'text/x-yaml' are supported, your mime-type : " + contentType)
+      // }
+      case "/reset" => {
+        Right(_compilerSrv.reset())
+      }
+      case "/clean" => {
+        Right(_compilerSrv.clean())
+      }
+      case "/compile" => {
+        Right(_compilerSrv.compile())
+      }
+      case "/clean-compile" => {
+        Right(_compilerSrv.clean() + "\n" + _compilerSrv.compile())
+      }
+      case unknown => {
+        println("unsupported operation : " + unknown)
+        Left(HttpResponseStatus.BAD_REQUEST)
+      }
+    }
+  }
+
+  private
+  def sendText(ctx : ChannelHandlerContext, txt : CharSequence) {
+    val response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
+    response.setHeader(HttpHeaders.Names.CONTENT_TYPE, "text/plain; charset=UTF-8")
+    //response.setHeader(HttpHeaders.Names.CONTENT_LENGTH, txt.length)
+    response.setContent(ChannelBuffers.copiedBuffer(txt.toString() + "\r\n", "UTF-8"))
+    ctx.getChannel().write(response).addListener(ChannelFutureListener.CLOSE)
+  }
+
+  private
+  def sendError(ctx : ChannelHandlerContext, status : HttpResponseStatus, exc : Throwable) {
+    val writer = new java.io.StringWriter()
+    exc.printStackTrace(new java.io.PrintWriter(writer))
+    writer.close()
+    sendError(ctx, status, writer.toString)
+  }
+
+  private
+  def sendError(ctx : ChannelHandlerContext, status : HttpResponseStatus, txt : CharSequence) {
+    val response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, status)
+    response.setHeader(HttpHeaders.Names.CONTENT_TYPE, "text/plain; charset=UTF-8")
+    response.setContent(ChannelBuffers.copiedBuffer("Failure: " + status.toString() + "\n" + txt.toString, "UTF-8"));
+    // Close the connection as soon as the error message is sent.
+    ctx.getChannel().write(response).addListener(ChannelFutureListener.CLOSE)
+  }
+}
+
+import org.jboss.netty.channel.ChannelPipelineFactory;
+
+/**
+ * @author The Netty Project (netty-dev@lists.jboss.org)
+ * @author Trustin Lee (tlee@redhat.com)
+ * @author david.bernard
+ * @based on code from org.jboss.netty.example.http.file
+ */
+class HttpStaticFileServerPipelineFactory extends ChannelPipelineFactory {
+
+  import org.jboss.netty.channel.Channels
+  //import org.jboss.netty.handler.stream.ChunkedWriteHandler;
+  import org.jboss.netty.channel.ChannelPipeline;
+  import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
+  import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
+  private
+  val _compilerSrv = new CompilerService4Group()
+
+  def getPipeline() : ChannelPipeline = {
+    // Create a default pipeline implementation.
+    val pipeline = Channels.pipeline()
+
+    // Uncomment the following line if you want HTTPS
+    //SSLEngine engine = SecureChatSslContextFactory.getServerContext().createSSLEngine();
+    //engine.setUseClientMode(false);
+    //pipeline.addLast("ssl", new SslHandler(engine));
+
+    pipeline.addLast("decoder", new HttpRequestDecoder())
+    pipeline.addLast("encoder", new HttpResponseEncoder())
+    //pipeline.addLast("chunkedWriter", new ChunkedWriteHandler())
+
+    pipeline.addLast("handler", new HttpStaticFileServerHandler(_compilerSrv))
+    pipeline
+  }
+}
---
+++ src/main/scala/org/scala_tools/server/utils.scala
@@ -0,0 +1,110 @@
+package org.scala_tools.server
+
+object RegExpUtil {
+  import java.util.ArrayDeque
+  import java.util.regex.{Pattern, PatternSyntaxException}
+
+  /**
+   * Return the compiled Java regex Pattern equivalent to the specified GLOB expression.
+   *
+   * @param glob GLOB expression to be compiled
+   * @return equivalent compiled Java regex Pattern
+   */
+  def globToRegexPattern(glob : String) : Pattern = {
+    /* Stack to keep track of the parser mode: */
+    /* "--" : Base mode (first on the stack)   */
+    /* "[]" : Square brackets mode "[...]"     */
+    /* "{}" : Curly braces mode "{...}"        */
+    val parserMode = new ArrayDeque[String]()
+    parserMode.push("--"); // base mode
+
+    val globLength = glob.length()
+    var index = 0 // index in glob
+
+    // equivalent REGEX expression to be compiled
+    val t = new StringBuilder()
+
+    while (index < globLength) {
+      var c = glob.charAt(index)
+      index += 1
+
+      if (c == '\\') {
+        // (1) ESCAPE SEQUENCE *
+        if (index == globLength) {
+          // no characters left, so treat '\' as literal char
+          t.append(Pattern.quote("\\"));
+        } else {
+          // read next character
+          c = glob.charAt(index)
+          val s = c + ""
+          if (("--".equals(parserMode.peek()) && "\\[]{}?*".contains(s)) ||
+              ("[]".equals(parserMode.peek()) && "\\[]{}?*!-".contains(s)) ||
+              ("{}".equals(parserMode.peek()) && "\\[]{}?*,".contains(s))) {
+            // escape the construct char
+            index += 1
+            t.append(Pattern.quote(s))
+          } else {
+            /* treat '\' as literal char */
+            t.append(Pattern.quote("\\"))
+          }
+        }
+      } else if (c == '*') {
+        // (2) GLOB PATTERN '*'
+        // create non-capturing group to match zero or more characters
+        t.append(".*")
+      } else if (c == '?') {
+        // (3) GLOB PATTERN '?'
+        // create non-capturing group to match exactly one character
+        t.append('.')
+      } else if (c == '[') {
+        // (4) GLOB PATTERN "[...]"
+        // opening square bracket '['
+        // create non-capturing group to match exactly one character
+        // inside the sequence
+        t.append('[')
+        parserMode.push("[]")
+        // check for negation character '!' immediately after the opening bracket '['
+        if ((index < globLength) && (glob.charAt(index) == '!')) {
+          index += 1;
+          t.append('^');
+        }
+      } else if ((c == ']') && "[]".equals(parserMode.peek())) {
+        // closing square bracket ']'
+        t.append(']');
+        parserMode.pop();
+      } else if ((c == '-') && "[]".equals(parserMode.peek())) {
+        // character range '-' in "[...]"
+        t.append('-')
+      } else if (c == '{') {
+        // (5) GLOB PATTERN "{...}"
+        // opening curly brace '{'
+        // create non-capturing group to match one of the
+        // strings inside the sequence
+        t.append("(?:(?:")
+        parserMode.push("{}")
+      } else if ((c == '}') && "{}".equals(parserMode.peek())) {
+        // closing curly brace '}'
+        t.append("))")
+        parserMode.pop()
+      } else if ((c == ',') && "{}".equals(parserMode.peek())) {
+        // comma between strings in "{...}"
+        t.append(")|(?:")
+      } else {
+        // (6) LITERAL CHARACTER
+        // convert literal character to a regex string
+        t.append(Pattern.quote(c + ""))
+      }
+    }
+    // done parsing all chars of the source pattern string
+
+    // check for mismatched [...] or {...}
+    if ("[]".equals(parserMode.peek())) {
+      throw new PatternSyntaxException("Cannot find matching closing square bracket ] in GLOB expression", glob, -1);
+    }
+
+    if ("{}".equals(parserMode.peek())) {
+      throw new PatternSyntaxException("Cannot find matching closing curly brace } in GLOB expression", glob, -1);
+    }
+    Pattern.compile(t.toString())
+  }
+}
