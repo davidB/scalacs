ee8a67317a9ed0feda3e9c9436b39131a1fa5bf5
David Bernard <david.bernard.31@gmail.com>
2010-03-05 21:21:06 +0100

    add testcase for compilation failed about syntax error

--- src/test/scala/RequestCompile.scala
+++ src/test/scala/RequestCompile.scala
@@ -3,8 +3,10 @@ package net_alchim31_scalacs
 class RequestCompileTest {
   import org.junit.{Before, Test}
   import java.net.URLClassLoader
+  import java.io.File
 
-  def trace(str : String) = {}
+  def trace(str : String) = { /* println(str) */ }
+  
   private lazy
   val _client = {
     import net_alchim31_scalacs_client.BasicHttpScalacsClient
@@ -20,14 +22,28 @@ class RequestCompileTest {
     }
   }
 
+  private
+  val _prjDir = System.getProperty("project.basedir", System.getProperty("user.dir"))
+  private
+  val _samplesClassesRootDir = new File(System.getProperty("samplesClassesRootDir", _prjDir +"/target/samples-classes"))
+  private
+  val _samplesSrcRootDir = new File(System.getProperty("samplesSrcRootDir", _prjDir +"/src/test/samples"))
+  private
+  val _scalaLibPath = {
+    val urls = Thread.currentThread.getContextClassLoader.asInstanceOf[URLClassLoader].getURLs
+    //val urls = this.getClass.getClassLoader.asInstanceOf[URLClassLoader].getURLs
+    trace(urls.mkString("[\n", "\n", "\n]"))
+    urls.filter(_.getPath.contains("scala-library")).firstOption.map(_.getPath).getOrElse{
+      throw new IllegalStateException("scala-library not found")
+    }
+  }
+
   @Test
   def compileHelloOkWithoutScalaLibShouldFailed() {
     import java.io.File
     import org.junit.Assert
     val sampleName = "prj-hello-ok" //-WithoutScalaLibShouldFailed"
-    val prjDir = System.getProperty("project.basedir", System.getProperty("user.dir"))
-    val samplesSrcRootDir = new File(System.getProperty("samplesSrcRootDir", prjDir +"/src/test/samples"))
-    val samplesClassesRootDir = new File(System.getProperty("samplesClassesRootDir", prjDir +"/target/samples-classes"))
+
     // register project
     val createReturn = _client.sendRequestCreateOrUpdate(String.format("""
 name : %s
@@ -39,17 +55,24 @@ excludes :
 targetDir : "%s"
 classpath :
 args :
-    """, sampleName, new File(samplesSrcRootDir, sampleName).getCanonicalPath, new File(samplesClassesRootDir, sampleName).getCanonicalPath));
+    """,
+      sampleName,
+      new File(_samplesSrcRootDir, sampleName).getCanonicalPath,
+      new File(_samplesClassesRootDir, sampleName).getCanonicalPath
+    ));
     trace(createReturn)
     Assert.assertTrue("one project added", createReturn.contains("created/updated/total : 1/0/1"))
     // request compile
-    val compileReturn = _client.sendRequestCompile()
-    trace(compileReturn)
-    Assert.assertTrue("has Error", compileReturn.contains("-ERROR\t"))
-    Assert.assertTrue("has good error message", compileReturn.contains("object scala not found"))
-    val removeReturn = _client.sendRequestRemove(sampleName);
-    trace(removeReturn)
-    Assert.assertTrue("every project removed", removeReturn.contains("removed/total : 1/0"))
+    try {
+      val compileReturn = _client.sendRequestCompile()
+      trace(compileReturn)
+      Assert.assertTrue("has Error", compileReturn.contains("-ERROR\t"))
+      Assert.assertTrue("has good error message", compileReturn.contains("object scala not found"))
+    } finally {
+      val removeReturn = _client.sendRequestRemove(sampleName);
+      trace(removeReturn)
+      Assert.assertTrue("every project removed", removeReturn.contains("removed/total : 1/0"))
+    }
   }
 
   @Test
@@ -58,15 +81,6 @@ args :
     import org.junit.Assert
     import java.net.URLClassLoader
     val sampleName = "prj-hello-ok"
-    val prjDir = System.getProperty("project.basedir", System.getProperty("user.dir"))
-    val samplesSrcRootDir = new File(System.getProperty("samplesSrcRootDir", prjDir +"/src/test/samples"))
-    val samplesClassesRootDir = new File(System.getProperty("samplesClassesRootDir", prjDir +"/target/samples-classes"))
-    val urls = Thread.currentThread.getContextClassLoader.asInstanceOf[URLClassLoader].getURLs
-    //val urls = this.getClass.getClassLoader.asInstanceOf[URLClassLoader].getURLs
-    trace(urls.mkString("[\n", "\n", "\n]"))
-    val scalaLibPath = urls.filter(_.getPath.contains("scala-library")).firstOption.map(_.getPath).getOrElse{
-      throw new IllegalStateException("scala-library not found")
-    }
     // register project
     val createReturn = _client.sendRequestCreateOrUpdate(String.format("""
 name : %s
@@ -81,20 +95,61 @@ classpath :
 args :
     """,
       sampleName,
-      new File(samplesSrcRootDir, sampleName).getCanonicalPath,
-      new File(samplesClassesRootDir, sampleName).getCanonicalPath,
+      new File(_samplesSrcRootDir, sampleName).getCanonicalPath,
+      new File(_samplesClassesRootDir, sampleName).getCanonicalPath,
       //urls.map('"' + _.getPath + '"').mkString("\n  - ")
-      scalaLibPath
+      _scalaLibPath
+    ));
+    trace(createReturn)
+    Assert.assertTrue("one project added", createReturn.contains("created/updated/total : 1/0/1"))
+    try {
+      // request compile
+      val compileReturn = _client.sendRequestCompile()
+      trace(compileReturn)
+      Assert.assertTrue("has no Error", !compileReturn.contains("-ERROR\t"))
+    } finally {
+      val removeReturn = _client.sendRequestRemove(sampleName);
+      trace(removeReturn)
+      Assert.assertTrue("every project removed", removeReturn.contains("removed/total : 1/0"))
+    }
+  }
+
+  @Test
+  def compileHelloFailedCompile() {
+    import java.io.File
+    import org.junit.Assert
+    val sampleName = "prj-hello-failed-compile" //-WithoutScalaLibShouldFailed"
+    // register project
+    val createReturn = _client.sendRequestCreateOrUpdate(String.format("""
+name : %s
+sourceDirs :
+  - "%s"
+includes :
+  - "*.scala"
+excludes :
+targetDir : "%s"
+classpath :
+  - %s
+args :
+    """,
+      sampleName,
+      new File(_samplesSrcRootDir, sampleName).getCanonicalPath,
+      new File(_samplesClassesRootDir, sampleName).getCanonicalPath,
+      _scalaLibPath
     ));
     trace(createReturn)
     Assert.assertTrue("one project added", createReturn.contains("created/updated/total : 1/0/1"))
     // request compile
-    val compileReturn = _client.sendRequestCompile()
-    trace(compileReturn)
-    Assert.assertTrue("has no Error", !compileReturn.contains("-ERROR\t"))
-    val removeReturn = _client.sendRequestRemove(sampleName);
-    trace(removeReturn)
-    Assert.assertTrue("every project removed", removeReturn.contains("removed/total : 1/0"))
+    try {
+      val compileReturn = _client.sendRequestCompile()
+      trace(compileReturn)
+      Assert.assertTrue("has Error", compileReturn.contains("-ERROR\t"))
+      Assert.assertTrue("has good error message", compileReturn.contains("')' expected but '}' found"))
+    } finally {
+      val removeReturn = _client.sendRequestRemove(sampleName);
+      trace(removeReturn)
+      Assert.assertTrue("every project removed", removeReturn.contains("removed/total : 1/0"))
+    }
   }
 
 }
